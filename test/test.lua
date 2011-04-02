1bec6e6e460344333c8a5cacc7abe82a4fe5171e
Alexander Gladysh <agladysh@gmail.com>
2011-04-03 02:10:14 +0400

    test: replaced luatexts_save with luatexts_lua.save(), uncommented tests

--- test/test.lua
+++ test/test.lua
@@ -1,5 +1,7 @@
 -- TODO: Scrap these hacks and write a proper test suite.
 
+package.path = "src/lua/?.lua;" .. package.path
+
 pcall(require, 'luarocks.require')
 require 'lua-nucleo.module'
 require 'lua-nucleo.strict'
@@ -8,6 +10,7 @@ require = import 'lua-nucleo/require_and_declare.lua' { 'require_and_declare' }
 math.randomseed(12345)
 
 local luatexts = require 'luatexts'
+local luatexts_lua = require 'luatexts.lua'
 
 local ensure,
       ensure_equals,
@@ -39,93 +42,7 @@ local tpretty = import 'lua-nucleo/tpretty.lua' { 'tpretty' }
 
 --------------------------------------------------------------------------------
 
--- TODO: Make a module out of this
-
-local luatexts_save
-do
-  local handlers = { }
-
-  local handle_value = function(cat, v, visited, buf)
-    local handler = handlers[type(v)]
-    if handler == nil then
-      return nil, "can't save `" .. type(v) .. "'"
-    end
-    return handler(cat, v, { }, buf)
-  end
-
-  handlers["nil"] = function(cat, v, visited, buf)
-    return cat "-" "\n"
-  end
-
-  handlers["boolean"] = function(cat, v, visited, buf)
-    return cat (v and "1" or "0") "\n"
-  end
-
-  handlers["number"] = function(cat, v, visited, buf)
-    return cat "N" "\n" (("%.54g"):format(v)) "\n"
-  end
-
-  handlers["string"] = function(cat, v, visited, buf)
-    return cat "S" "\n" (#v) "\n" (v) "\n"
-  end
-
-  handlers["table"] = function(cat, t, visited, buf)
-    if visited[t] then
-      -- TODO: This should be `return nil, err`, not `error()`!
-      error("circular table reference detected")
-    end
-    visited[t] = true
-
-    cat "T" "\n"
-
-    local array_size = #t
-    cat (array_size) "\n"
-
-    local hash_size_pos = #buf + 1
-    cat ("?") "\n"
-
-    for i = 1, array_size do
-      handle_value(cat, t[i], visited, buf)
-    end
-
-    local hash_size = 0
-    for k, v in pairs(t) do
-      if
-        type(k) ~= "number" or
-        k > array_size or k < 1 or -- integer key in hash part of the table
-        k % 1 ~= 0 -- non-integer key
-      then
-        hash_size = hash_size + 1
-        handle_value(cat, k, visited, buf)
-        handle_value(cat, v, visited, buf)
-      end
-    end
-
-    buf[hash_size_pos] = hash_size
-
-    visited[t] = nil
-
-    return cat
-  end
-
-  luatexts_save = function(...)
-    local nargs = select("#", ...)
-    local buf = { }
-    local function cat(s) buf[#buf + 1] = s; return cat end
-
-    cat (nargs) "\n"
-
-    for i = 1, nargs do
-      handle_value(cat, select(i, ...), { }, buf)
-    end
-
-    return table.concat(buf)
-  end
-end
-
---------------------------------------------------------------------------------
-
-for NAME, NL in pairs { LF = "\n", CRLF = "\r\n" } do --[===[
+for NAME, NL in pairs { LF = "\n", CRLF = "\r\n" } do
   print("===== BEGIN core tests", NAME, "=====")
 
   ensure_fails_with_substring(
@@ -1883,7 +1800,7 @@ end)()
       expected_errors
     )
 
-  print("===== END utf8 tests", NAME, "=====") --]===]
+  print("===== END utf8 tests", NAME, "=====")
   print("===== BEGIN table tests", NAME, "=====")
 
   ensure_returns(
@@ -2122,8 +2039,9 @@ end)()
         tuple[i] = construct()
       end
 
-      local data = ensure("save", luatexts_save(unpack(tuple, 1, n)))
+      local data = ensure("save", luatexts_lua.save(unpack(tuple, 1, n)))
 
+      --[[
       print("dataset #" .. i .. ", " .. #data .. " bytes")
       local c = 0
       print("tuple: ", tpretty(tuple, '  ', 80))
@@ -2136,6 +2054,7 @@ end)()
               end
             )
         )
+      --]]
 
       -- TODO: This does not cover utf-8. It should!
       ensure_returns(
