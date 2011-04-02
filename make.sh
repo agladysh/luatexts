f0f3ec5a4c29666835db0332a0fe9139521e81f6
Alexander Gladysh <agladysh@gmail.com>
2011-04-03 01:45:35 +0400

    make.sh: added pedantic stage

--- make.sh
+++ make.sh
@@ -6,6 +6,17 @@ if ([ -z "${CLOSURE}" ]); then
   CLOSURE="$(luarocks show --rock-dir pk-core-js.google-closure-compiler)"
 fi
 
+echo "----> Going pedantic all over the source"
+
+echo "--> c89..."
+gcc -O2 -fPIC -I/usr/include/lua5.1 -c src/c/luatexts.c -o /dev/null -Isrc/c/ -Wall --pedantic -Werror --std=c89
+
+echo "--> c99..."
+gcc -O2 -fPIC -I/usr/include/lua5.1 -c src/c/luatexts.c -o /dev/null -Isrc/c/ -Wall --pedantic -Werror --std=c99
+
+echo "--> c++98..."
+gcc -xc++ -O2 -fPIC -I/usr/include/lua5.1 -c src/c/luatexts.c -o /dev/null -Isrc/c/ -Wall --pedantic -Werror --std=c++98
+
 echo "----> Making rock"
 sudo luarocks make rockspec/luatexts-scm-1.rockspec
 
